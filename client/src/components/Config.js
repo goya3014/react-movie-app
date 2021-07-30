d6b0b89333b1e9ed1226154f4eb9f425fa7afd76
unknown <goya3014@hanmail.net>
2021-07-30 23:46:21 +0900

    LandingPage 작업중

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,9 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+//theMovieAPI의 공통주소 부분을 상수처리
+export const API_URL = 'https://api.themoviedb.org/3/';
+//API KEY도 상수처리
+export const API_KEY = 'ebbb7bbc1ced17274f2be9b788695243';
+
+//IMAGE API의 공통부분도 상수처리
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/'; 
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { FaCode } from "react-icons/fa";
-
+import {API_URL, API_KEY } from '../../Config';
+import MainImage from './Sections/MainImage';
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        //fetch 함수를 통해 endpoint를 넣어 인기있는 영화 가져오기
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            setMovies([response.results])
+            //MainMovieImage에 가장 잘나가는 영화인 첫번째 영화를 넣어주기
+            setMainMovieImage(response.results[0])
+        })
+
+    }, [])
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '100%', margin: '0' }}>
+                {/* Main Image */}
+                <MainImage />
+            <div style={{ width: '85%', margin: '1rem auto'}}>
+                <h2>Movies by latest</h2>
+                <hr />
+                {/* Movie Grid Cards */}
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <button>Load More</button>
+            </div>
+        </div>
     )
 }
 
