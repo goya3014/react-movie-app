3395f58b9ab0d56e91e8d4ec401317cfeead8fd2
unknown <goya3014@hanmail.net>
2021-08-01 20:03:34 +0900

    GridCard 컴포넌트 완성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import { FaCode } from "react-icons/fa";
 import {API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import {Row} from 'antd';
 function LandingPage() {
 
     const [Movies, setMovies] = useState([])
@@ -13,10 +15,10 @@ function LandingPage() {
         fetch(endpoint)
         .then(response => response.json())
         .then(response => {
-            setMovies([response.results])
+            setMovies(...[response.results])
             //MainMovieImage에 가장 잘나가는 영화인 첫번째 영화를 넣어주기
             setMainMovieImage(response.results[0])
-            console.log(response.results)
+  
         })
 
     }, [])
@@ -34,6 +36,18 @@ function LandingPage() {
                 <h2>Movies by latest</h2>
                 <hr />
                 {/* Movie Grid Cards */}
+                <Row gutter={[16, 16]}>
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                           <GridCards image={movie.poster_path ?
+                                            `${IMAGE_BASE_URL}w500${movie.poster_path}` : null }
+                                      movieId={movie.id}
+                                      movieName={movie.original_title} 
+                           />     
+                        </React.Fragment>
+                    ))}
+                    
+                </Row>
             </div>
             <div style={{display: 'flex', justifyContent: 'center'}}>
                 <button>Load More</button>
